41c05d29f2ea1f2b05def7415072f2004c27011b
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-10-21 12:20:39 +0300

    User Interface

--- app/src/main/java/com/example/instagramclone/FeedActivity.java
+++ app/src/main/java/com/example/instagramclone/FeedActivity.java
@@ -3,24 +3,12 @@ package com.example.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-    
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void signInClicked(View view){
-
-    }
-
-    public void signUpClicked(View view){
-
+        setContentView(R.layout.activity_feed);
     }
 }

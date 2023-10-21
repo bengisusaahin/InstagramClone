ee005a6a56e65761b13c9ff1df0179934706e3a7
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-10-21 20:44:13 +0300

    Signout Operations

--- app/src/main/java/com/example/instagramclone/FeedActivity.java
+++ app/src/main/java/com/example/instagramclone/FeedActivity.java
@@ -1,14 +1,50 @@
 package com.example.instagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        auth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        
+        if (item.getItemId() == R.id.add_post){
+            //Upload activity
+            Intent intentToUpload = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentToUpload);
+            //finish demeye gerek yok cunku kullanici upload yapmaktan vazgecebilir
+        } else if (item.getItemId() == R.id.signout) {
+            //Signout
+            auth.signOut();
+
+            Intent intentToMain = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToMain);
+            finish(); //kullanici cikis yaptiysa buraya geri donememesi lazim
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }

5df012205977e0945ef8d41f35c9191465c4fd82
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-10-22 21:00:00 +0300

    Pull data

--- app/src/main/java/com/example/instagramclone/FeedActivity.java
+++ app/src/main/java/com/example/instagramclone/FeedActivity.java
@@ -1,6 +1,7 @@
 package com.example.instagramclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,17 +9,56 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getData();
+    }
+
+    private void getData(){
+        //DocumentReference documentReference = firebaseFirestore.collection("Posts").document("fgs");
+        //CollectionReference documentReference = firebaseFirestore.collection("Posts");
+
+        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    Toast.makeText(FeedActivity.this,error.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }
+                if (value != null){
+                    for (DocumentSnapshot snapshot : value.getDocuments()){
+                        Map<String, Object> data = snapshot.getData();
+
+                        //Casting
+                        String userEmail = (String) data.get("useremail");
+                        String comment = (String) data.get("comment");
+                        String downloadUrl = (String) data.get("downloadurl");
+
+                        System.out.println(comment);
+                    }
+                }
+            }
+        });
     }
 
     @Override
